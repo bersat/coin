f98c78a20582d01bf529a70395575c563e590760
bersat <doom_fox754@mail.ru>
2023-09-25 23:00:23 +0300

    osnova

--- stranica1/main.js
+++ stranica1/main.js
@@ -10,6 +10,7 @@ const bank = document.querySelector('#bank')
 function start() {
     btn.onclick = () => bank.textContent = ++x; 
 }
+
 start();
 
 
@@ -17,26 +18,14 @@ start();
 
     const mexsik = document.querySelector('.mexsik')
 const classic = document.querySelector('#classic')
-<<<<<<< HEAD
 
 
 
     mexsik.onclick = () => {document.querySelector('#maincoin1').src = "imageredact/мексиканский-coin.png";
     document.querySelector('#maincoin2').src = "imageredact/мексиканский-coin.png";
     };
-=======
-
-    mexsik.onclick = () => {document.querySelector('#maincoin1').src = "imageredact/мексиканский-coin.png";
-    document.querySelector('#maincoin2').src = "imageredact/мексиканский-coin.png";
-}
->>>>>>> 2c6cd89399b3859576b01c32095393f7a7138749
 
     classic.onclick = () => {
         document.querySelector('#maincoin1').src = "imageredact/классик.png";
     document.querySelector('#maincoin2').src = "imageredact/классик.png";
-<<<<<<< HEAD
     } ;
-=======
-    }
-    
->>>>>>> 2c6cd89399b3859576b01c32095393f7a7138749
